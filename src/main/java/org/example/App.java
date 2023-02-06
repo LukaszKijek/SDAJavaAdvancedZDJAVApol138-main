e40601e5e23f3b2ccee0e5994a70b61ad009474a
LukaszKijek <lukaskijek@gmail.com>
2023-02-06 19:02:06 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.Collections.MapExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        MapExample.createMapOfSalaries();
+    }
+}
